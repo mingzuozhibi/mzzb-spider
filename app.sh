9ea0e8ea8c73a0e343253d36e0c96fc0efd5c4f6
fuhaiwei <fuhaiwei@126.com>
2019-09-12 12:31:47 +0800

    feat: Update Startup Script

--- app.sh
+++ app.sh
@@ -15,22 +15,20 @@ cd ${APP_HOME}
 
 JAVA_OPTS="-Xms64m -Xmx128m -XX:MaxMetaspaceSize=1024m"
 JAR_OPTS="--app.name=${APP_NAME} --spring.profiles.active=pro"
-LOG_FILE="target/webapp.log"
 
-if [ -f ./RUN_OPTS ]; then
-  source ./RUN_OPTS
-fi
+STD_FILE="target/std.log"
+WEB_FILE="target/web.log"
 
 start_app() {
-  if [ -z "$(jar_file)" ]; then
+  if [[ -z "$(jar_file)" ]]; then
     build_app
   fi
-  if [ -z "$(jar_file)" ]; then
+  if [[ -z "$(jar_file)" ]]; then
     echo "启动应用失败：未找到JAR文件"
   else
     echo "正在启动应用"
-    echo "java ${JAVA_OPTS} -jar $(jar_file) ${JAR_OPTS} >${LOG_FILE} 2>&1"
-    nohup java ${JAVA_OPTS} -jar $(jar_file) ${JAR_OPTS} >${LOG_FILE} 2>&1 & exit 0
+    echo "java ${JAVA_OPTS} -jar $(jar_file) ${JAR_OPTS} >${STD_FILE} 2>&1"
+    nohup java ${JAVA_OPTS} -jar $(jar_file) ${JAR_OPTS} >${STD_FILE} 2>&1 & exit 0
   fi
 }
 
@@ -65,7 +63,7 @@ unsupport() {
 # 执行指定脚本命令
 ##
 
-if [ $# = 1 ]; then
+if [[ $# = 1 ]]; then
   case $1 in
     help)
       echo "usage: run.sh [<command>] [<args>]"
@@ -77,23 +75,26 @@ if [ $# = 1 ]; then
       echo "   qt                停止应用"
       echo "   qt -f             停止应用（强制停止）"
       echo "   vt                运行状态"
-      echo ""
-      echo "   sl                tail -f <Log>"
-      echo "   sl <pattern>      grep <pattern> Log"
-      echo "   sl -a             less <Log>"
-      echo "   sl -v             vim Log"
+      echo "   jm -h             jmap -heap <pid>"
       echo ""
       echo "   fe -m             git fetch && git reset --hard origin/master"
       echo "   fe -d             git fetch && git reset --hard origin/develop"
       echo "   fe <branch>       git fetch && git reset --hard origin/<branch>"
       echo ""
-      echo "   jm -h             jmap -heap <pid>"
+      echo "   std                tail -f ${STD_FILE}"
+      echo "   std -a             less ${STD_FILE}"
+      echo "   std <pattern>      grep <pattern> ${STD_FILE}"
+      echo ""
+      echo "   web                tail -f ${WEB_FILE}"
+      echo "   web -a             less ${WEB_FILE}"
+      echo "   web <pattern>      grep <pattern> ${WEB_FILE}"
+      echo ""
       echo "   help              显示本帮助"
       echo ""
       exit 0
     ;;
     st)
-      if [ -n "$(app_pid)" ]; then
+      if [[ -n "$(app_pid)" ]]; then
         echo "应用已经启动"
         exit 1
       else
@@ -102,7 +103,7 @@ if [ $# = 1 ]; then
       fi
     ;;
     qt)
-      if [ -z "$(app_pid)" ]; then
+      if [[ -z "$(app_pid)" ]]; then
         echo "应用并未运行"
         exit 1
       else
@@ -111,7 +112,7 @@ if [ $# = 1 ]; then
       fi
     ;;
     vt)
-      if [ -n "$(app_pid)" ]; then
+      if [[ -n "$(app_pid)" ]]; then
         echo "应用运行中, PID=$(app_pid)"
         exit 0
       else
@@ -119,19 +120,24 @@ if [ $# = 1 ]; then
         exit 0
       fi
     ;;
-    sl)
-      echo "正在打开日志: tail -f ${LOG_FILE}"
-      tail -f ${LOG_FILE}
+    std)
+      echo "正在打开日志: tail -f ${STD_FILE}"
+      tail -f ${STD_FILE}
+      exit 0
+    ;;
+    web)
+      echo "正在打开日志: tail -f ${WEB_FILE}"
+      tail -f ${WEB_FILE}
       exit 0
     ;;
   esac
-elif [ $# = 2 ]; then
+elif [[ $# = 2 ]]; then
   case $1 in
     st)
-      if [ $2 = "-f" ]; then
+      if [[ $2 = "-f" ]]; then
         build_app
 
-        if [ -n "$(app_pid)" ]; then
+        if [[ -n "$(app_pid)" ]]; then
           kill_app_focus
         fi
 
@@ -140,8 +146,8 @@ elif [ $# = 2 ]; then
       fi
     ;;
     qt)
-      if [ $2 = "-f" ]; then
-        if [ -z "$(app_pid)" ]; then
+      if [[ $2 = "-f" ]]; then
+        if [[ -z "$(app_pid)" ]]; then
           echo "应用并未运行"
           exit 1
         else
@@ -150,11 +156,11 @@ elif [ $# = 2 ]; then
         fi
       fi
     ;;
-    sl)
+    jm)
+      pid=$(app_pid)
+      if [[ -z "${pid}" ]]; then echo "应用并未运行"; exit 1; fi
       case $2 in
-        -a) less ${LOG_FILE}; exit 0;;
-        -v) vim ${LOG_FILE}; exit 0;;
-        *) grep $2 ${LOG_FILE}; exit 0;;
+        -h) jmap -heap ${pid}; exit 0;;
       esac
     ;;
     fe)
@@ -164,11 +170,16 @@ elif [ $# = 2 ]; then
         *) git fetch && git reset --hard origin/$2; exit 0;;
       esac
     ;;
-    jm)
-      pid=$(app_pid)
-      if [ -z "${pid}" ]; then echo "应用并未运行"; exit 1; fi
+    std)
       case $2 in
-        -h) jmap -heap ${pid}; exit 0;;
+        -a) less ${STD_FILE}; exit 0;;
+        *) grep $2 ${STD_FILE}; exit 0;;
+      esac
+    ;;
+    web)
+      case $2 in
+        -a) less ${WEB_FILE}; exit 0;;
+        *) grep $2 ${WEB_FILE}; exit 0;;
       esac
     ;;
   esac
