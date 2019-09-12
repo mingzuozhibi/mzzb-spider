934936374ca94254c7f2b7393e175d244e2984f5
fuhaiwei <fuhaiwei@126.com>
2019-09-12 12:36:10 +0800

    feat: Remove Text Domain

--- src/main/java/mingzuozhibi/discspider/domain/TextRepository.java
+++
@@ -1,9 +0,0 @@
-package mingzuozhibi.discspider.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TextRepository extends JpaRepository<Text, Long> {
-
-    Text getOneByName(String name);
-
-}
