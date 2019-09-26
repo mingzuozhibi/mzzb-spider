f960d6b2cccb51bf91962f753d475a95ee054a77
fuhaiwei <fuhaiwei@126.com>
2019-09-26 18:30:43 +0800

    feat(parser): Check empty title

--- src/main/java/mingzuozhibi/discspider/DiscParser.java
+++ src/main/java/mingzuozhibi/discspider/DiscParser.java
@@ -65,6 +65,7 @@ public class DiscParser {
 
         if (StringUtils.isEmpty(disc.getTitle())) {
             jmsMessage.warning("解析信息：[%s][未发现碟片标题]", asin);
+            return Optional.empty();
         }
 
         /*
