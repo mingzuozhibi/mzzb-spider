3bc29e9bcc0e0906d03b378c08eaa43857e40640
fuhaiwei <fuhaiwei@126.com>
2021-08-03 22:35:20 +0800

    feat: use cdp4j for fetch discs

--- src/main/java/mingzuozhibi/discspider/DiscSpider.java
+++ src/main/java/mingzuozhibi/discspider/DiscSpider.java
@@ -1,5 +1,6 @@
 package mingzuozhibi.discspider;
 
+import io.webfolder.cdp.session.SessionFactory;
 import lombok.extern.slf4j.Slf4j;
 import mingzuozhibi.common.jms.JmsMessage;
 import mingzuozhibi.common.model.Result;
@@ -17,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static mingzuozhibi.common.spider.SpiderCdp4j.doInSessionFactory;
+import static mingzuozhibi.common.spider.SpiderCdp4j.waitResult;
 import static mingzuozhibi.common.spider.SpiderRecorder.writeContent;
 
 @Slf4j
@@ -33,9 +36,7 @@ public class DiscSpider {
 
     public Result<Disc> updateDisc(String asin) {
         SpiderRecorder recorder = new SpiderRecorder("碟片信息", 1, jmsMessage);
-        Result<Disc> result = new Result<>();
-        result.syncResult(doUpdateDisc(recorder, asin));
-        return result;
+        return doUpdateDisc(null, recorder, asin);
     }
 
     public Map<String, Disc> updateDiscs(List<String> asins) {
@@ -43,63 +44,33 @@ public class DiscSpider {
         recorder.jmsStartUpdate();
 
         Map<String, Disc> discInfos = new LinkedHashMap<>();
-        for (String asin : asins) {
-            threadSleep(4000);
-            if (recorder.checkBreakCount(5)) break;
-            Result<Disc> result = doUpdateDisc(recorder, asin);
-            if (!result.isUnfinished()) {
-                discInfos.put(asin, result.getContent());
-            } else {
-                if (result.getErrorMessage().contains("code=503")) {
-                    threadSleep(10000);
-                }
-                if (result.getErrorMessage().contains("日亚反爬虫")) {
-                    threadSleep(10000);
+        doInSessionFactory(factory -> {
+            for (String asin : asins) {
+                if (recorder.checkBreakCount(5)) break;
+                Result<Disc> result = doUpdateDisc(factory, recorder, asin);
+                if (!result.isUnfinished()) {
+                    discInfos.put(asin, result.getContent());
                 }
             }
-        }
+        });
 
         recorder.jmsSummary();
         recorder.jmsEndUpdate();
         return discInfos;
     }
 
-    private void threadSleep(int ms) {
-        try {
-            Thread.sleep(ms);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     @SuppressWarnings("unchecked")
-    private Result<Disc> doUpdateDisc(SpiderRecorder recorder, String asin) {
+    private Result<Disc> doUpdateDisc(SessionFactory factory, SpiderRecorder recorder, String asin) {
         // 记录开始
         recorder.jmsStartUpdateRow(asin);
 
         // 开始抓取
         String url = "https://www.amazon.co.jp/dp/" + asin + "?language=ja_JP";
-
-        Result<String> bodyResult = new Result<>();
-        try {
-            Response execute = Jsoup.connect(url)
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
-                .referrer("https://www.google.com/")
-                .ignoreContentType(true)
-                .maxBodySize(10 * 1024 * 1024)
-                .execute();
-            bodyResult.setContent(execute.body());
-        } catch (Exception e) {
-            if (e instanceof HttpStatusException) {
-                HttpStatusException he = (HttpStatusException) e;
-                if (he.getStatusCode() == 404) {
-                    return maybeOffTheShelf(recorder, asin);
-                }
-                String newMessage = he.getMessage() + ", code=" + he.getStatusCode();
-                bodyResult.pushError(new HttpStatusException(newMessage, he.getStatusCode(), he.getUrl()));
-            } else {
-                bodyResult.pushError(e);
-            }
+        Result<String> bodyResult;
+        if (factory != null) {
+            bodyResult = waitResult(factory, url);
+        } else {
+            bodyResult = waitResultJsoup(url);
         }
 
         // 抓取失败
@@ -111,9 +82,7 @@ public class DiscSpider {
         String content = bodyResult.getContent();
 
         // 发现反爬
-        if (
-
-            hasAmazonNoSpider(content)) {
+        if (hasAmazonNoSpider(content)) {
             if (content.contains("何かお探しですか？")) {
                 return maybeOffTheShelf(recorder, asin);
             } else {
@@ -140,15 +109,35 @@ public class DiscSpider {
             recorder.jmsSuccessRow(asin, prevRank + " => " + thisRank);
             return Result.ofContent(disc);
 
-        } catch (
-            Exception e) {
+        } catch (Exception e) {
             // 捕获异常
             recorder.jmsErrorRow(asin, e);
             writeContent(content, asin);
             log.warn("parsing error", e);
             return Result.ofErrorCause(e);
         }
+    }
 
+    private Result<String> waitResultJsoup(String url) {
+        Result<String> result = new Result<>();
+        try {
+            Response execute = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
+                .referrer("https://www.google.com/")
+                .ignoreContentType(true)
+                .maxBodySize(10 * 1024 * 1024)
+                .execute();
+            result.setContent(execute.body());
+        } catch (Exception e) {
+            if (e instanceof HttpStatusException) {
+                HttpStatusException he = (HttpStatusException) e;
+                String newMessage = he.getMessage() + ", code=" + he.getStatusCode();
+                result.pushError(new HttpStatusException(newMessage, he.getStatusCode(), he.getUrl()));
+            } else {
+                result.pushError(e);
+            }
+        }
+        return result;
     }
 
     private Result<Disc> maybeOffTheShelf(SpiderRecorder recorder, String asin) {
