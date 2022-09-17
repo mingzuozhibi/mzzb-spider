47dbecb89a5fc14adc6c7c3c9617ea243b1ca992
fuhaiwei <fuhaiwei@126.com>
2022-09-17 19:42:16 +0800

    feat: Fix fetch start log

--- src/main/java/com/mingzuozhibi/TestController.java
+++ src/main/java/com/mingzuozhibi/TestController.java
@@ -10,9 +10,6 @@ import com.mingzuozhibi.support.SpiderCdp4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 import static com.mingzuozhibi.commons.base.BaseKeys.*;
 import static com.mingzuozhibi.support.SpiderUtils.waitResult;
 
@@ -24,19 +21,12 @@ public class TestController extends BaseController {
     private ContentSpider contentSpider;
 
     @GetMapping("/test/{asin}")
-    public void contentSearch(@PathVariable String asin) {
+    public void fetchContent(@PathVariable String asin) {
         var task = new SearchTask<Content>(asin);
         SpiderCdp4j.doInSessionFactory(factory -> {
             var recorder = new JmsRecorder(bind, "碟片信息", 1);
-            var bodyResult = waitResult(factory, task.getKey());
-            if (bodyResult.isSuccess()) {
-                try (var pw = new PrintWriter("var/output.html")) {
-                    pw.println(bodyResult.getData());
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            var result = contentSpider.fetchContent(recorder, task, bodyResult);
+            var result = contentSpider.fetchContent(recorder, task,
+                () -> waitResult(factory, task.getKey()));
             amqpSender.send(CONTENT_RETURN, gson.toJson(result));
         });
     }
