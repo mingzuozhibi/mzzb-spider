c35b2e4a4b098aedcc1bb7d7ac5b71dadec2c534
fuhaiwei <fuhaiwei@126.com>
2023-05-21 06:45:41 +0800

    feat: Not use random url

--- src/main/java/com/mingzuozhibi/support/SpiderUtils.java
+++ src/main/java/com/mingzuozhibi/support/SpiderUtils.java
@@ -5,7 +5,6 @@ import io.webfolder.cdp.session.Session;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Random;
 import java.util.function.Supplier;
 
 import static com.mingzuozhibi.support.SpiderCdp4j.waitResultCdp4j;
@@ -35,24 +34,7 @@ public abstract class SpiderUtils {
     }
 
     public static String buildUrl(String asin) {
-        return "https://www.amazon.co.jp/" + randomWord(35) + "/dp/" + asin + "/?" +
-            randomWord(3) + "=" + randomWord(4) + "&" +
-            "language=ja_JP&" +
-            randomWord(5) + "=" + randomWord(6) + "&" +
-            "_encoding=UTF8&" +
-            randomWord(7) + "=" + randomWord(8);
-    }
-
-    private static String randomWord(int count) {
-        var builder = new StringBuilder();
-        for (var i = 0; i < count; i++) {
-            builder.append(randomChar());
-        }
-        return builder.toString();
-    }
-
-    private static char randomChar() {
-        return (char) (new Random().nextInt(26) + 'a');
+        return "https://www.amazon.co.jp/dp/%s?language=ja_JP#detailBullets_feature_div".formatted(asin);
     }
 
 }
